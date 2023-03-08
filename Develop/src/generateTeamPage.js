7d865060c384b0dcd652d12e80cd171b11ce2aba
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-03-08 04:56:36 -0600

    added font awesome, bootstrap, and functions to pull data for team members and put onto html page

--- Develop/src/generateTeamPage.js
+++ Develop/src/generateTeamPage.js
@@ -1,10 +1,80 @@
-`
+const generateTeamPage = (myTeam) => {
+    console.log(myTeam);
+    // Empty array to push html elements and loop through team data
+    const html = [];
+    // Functions for each team member -- need these to return html data
+    const generateManager = manager => {
+        console.log(manager);
+        const managerHtml = ` 
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+                ${manager.name} <br>
+                <i class="fas fa-mug-hot"></i>Manager
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${manager.id}</li>
+                <li class="list-group-item">Email: <span id="email"><a href="mailto:${manager.email}">${manager.email}</a></span></li>
+                <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+            </ul>
+        </div>
+        `;
+        html.push(managerHtml);
+    }
+
+    const generateEngineer = engineer => {
+        console.log(engineer);
+        const engineerHtml = ` 
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+                ${engineer.name} <br>
+                <i class="fas fa-glasses"></i>Engineer
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${engineer.id}</li>
+                <li class="list-group-item">Email: <span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
+                <li class="list-group-item">Office Number: ${engineer.gitHubUsername}</li>
+            </ul>
+        </div>
+        `;
+        html.push(engineerHtml);
+    }
+
+    const generateIntern = intern => {
+        console.log(intern);
+        const internHtml = ` 
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+                ${intern.name} <br>
+                <i class="fas fa-user-graduate"></i>Intern
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${intern.id}</li>
+                <li class="list-group-item">Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
+                <li class="list-group-item">Office Number: ${intern.school}</li>
+            </ul>
+        </div>
+        `;
+        html.push(internHtml);
+    }
+}
+
+
+
+
+
+
+// Export function
+module.exports = myTeam => {
+    return `
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Team Profile Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/1e0a13a89f.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../dist/style.css">
 </head>
 <body>
@@ -12,8 +82,9 @@
         <h1>My Team</h1>
     </header>
     <main>
-        // ${generateTeam(team)} 
+        // ${generateTeamPage(myTeam)} 
     </main>
 </body>
 </html>
 `;
+}
