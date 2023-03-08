989497987e6b2b588faf9e45088ce235ab7da286
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-03-08 03:54:52 -0600

    tried to layout js code better in index.js; created functions to initialize app and question prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,8 @@
 // Set required npm's
 const inquirer = require('inquirer');
 const fs = require('fs');
+// const fs = require("fs");
+// const path = require("path");
 
 // Import lib.js files
 const Manager = require('./lib/Manager');
@@ -9,65 +11,72 @@ const Intern = require('./lib/Intern');
 const questionsManager = require('./lib/questions.Manager');
 const questionsEngineer = require('./lib/questions.Engineer');
 const questionsIntern = require('./lib/questions.Intern');
-const { log } = require('console');
 
 // Array of team member's when team is complete
-const myTeamGenerated = []
-
-// Initialize the application
+const genereatedTeamArray = [];
 
 
-// New team member question
-const addNewEmployeeQuestion = [
+// New team member question prompt
+const newEmployeePrompt = [
     {
         type: 'list',
         message: 'What\'s the role of the employee you want to add to your team?',
         name: 'role',
         choices: ['Manager', 'Engineer', 'Intern', 'My team is complete!']
-    }
-]
+    }]
 
-const addNewEmployee = () => {
-    inquirer.prompt(addNewEmployeeQuestion)
+const addEmployee = () => {
+    inquirer.prompt(newEmployeePrompt)
         .then((answer) => {
             if ('My team is complete!') {
-                console.log(myTeamGenerated);
+                console.log(genereatedTeamArray);
                 // console.log(------generate html function----------);
             } else if (answer.role === 'Manager') {
                 inquirer.prompt(questionsManager)
                     .then((answers) => {
                         const answersManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-                        myTeamGenerated.push(answersManager);
+                        genereatedTeamArray.push(answersManager);
                         console.log(answersManager);
-                        console.log(myTeamGenerated[0]);
-                        addNewEmployee();
+                        console.log(genereatedTeamArray[0]);
+                        addEmployee();
                     })
             } else if (answer.role === 'Engineer') {
                 inquirer.prompt(questionsEngineer)
                     .then((answers) => {
                         const answersEngineer = new Engineer(answers.name, answers.id, answers.email, answers.gitHubUsername);
-                        myTeamGenerated.push(answersEngineer);
+                        genereatedTeamArray.push(answersEngineer);
                         console.log(answersEngineer);
-                        console.log(myTeamGenerated[1]);
-                        addNewEmployee();
+                        console.log(genereatedTeamArray[1]);
+                        addEmployee();
                     })
             } else if (answer.role === 'Intern') {
                 inquirer.prompt(questionsIntern)
                     .then((answers) => {
                         const answersIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
-                        myTeamGenerated.push(answersIntern);
+                        genereatedTeamArray.push(answersIntern);
                         console.log(answersIntern);
-                        console.log(myTeamGenerated[2]);
-                        addNewEmployee();
+                        console.log(genereatedTeamArray[2]);
+                        addEmployee();
                     })
             }
         })
 }
 
+// Initialize the application
+const init = async () => {
+    await inquirer.prompt(questionsManager)
+        .then((answers) => {
+            const answersManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            genereatedTeamArray.push(answersManager);
+            console.log(answersManager);
+            console.log(genereatedTeamArray[0]);
+            addEmployee();
+        })
+};
+
 // Add function to generate html and write to file
-function generateFinishedTeam () {
+function generateMyTeam() {
+};
 
-}
-
-init ()
+init()
 
